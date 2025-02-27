0bfc1bba1a6e2d6eae16d4486b8bc53a7bdc8090
Ravi Gurjar <99577613+RaviHackjr@users.noreply.github.com>
2025-02-27 15:59:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,9 +12,9 @@ from logging.handlers import RotatingFileHandler
 #Bot token @Botfather
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "8075157208:AAGvt_0jj7cmzt-q7oWic_Rj0caY32tt1H4")
 #Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", ""))
+APP_ID = int(os.environ.get("APP_ID", "25096759"))
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "e2231e16629ec94fc6898d540250e369")
 #Your db channel Id
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002352398561"))
 # NAMA OWNER
@@ -24,7 +24,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "7753822898"))
 #Port
 PORT = os.environ.get("PORT", "8030")
 #Database
-DB_URI = os.environ.get("DATABASE_URL", "")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Blakite_Ravii:Ravi8962@cluster0.vdv6h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 
 #Time in seconds for message delete, put 0 to never delete
