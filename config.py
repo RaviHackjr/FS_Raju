201cced3c44bc3757b3a6e8bfbbc19e6b9ea1bcb
Ravi Gurjar <99577613+RaviHackjr@users.noreply.github.com>
2025-03-28 13:23:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ DB_NAME = os.environ.get("DATABASE_NAME", "AutoAnimeFileBot")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = os.environ.get("FORCE_SUB_CHANNEL", "@seishiro_at_anime_engsub")
-FORCE_SUB_CHANNEL2 = os.environ.get("FORCE_SUB_CHANNEL2", "@seishiro_anime_is")
+FORCE_SUB_CHANNEL2 = os.environ.get("FORCE_SUB_CHANNEL2", "@seishiro_anime_is_hindidub")
 #FORCE_SUB_CHANNEL3 = os.environ.get("FORCE_SUB_CHANNEL3", "@seishiro_at_anime_engsub")
 #FORCE_SUB_CHANNEL4 = os.environ.get("FORCE_SUB_CHANNEL4", "@seishiro_anime_is_hindidub")
 
